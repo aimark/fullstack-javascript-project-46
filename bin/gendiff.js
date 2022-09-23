499a385326e13461bffd6fb26a1e856ca1e9bbbc
Андрей Марк <markandreym@gmail.com>
2022-09-23 15:59:14 +0300

    Change lesson2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,9 @@ const program = new Command();
 program
   .name('gendif')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0');
+  .version('0.8.0')
+  .argument('<filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format');
 
-program.parse();
+program.parse(process.argv);
